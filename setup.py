8f387df2a07058e83cfe4e6fcf2d292dc66b1b5f
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-07-25 17:33:41 +0530

    Bump version to 0.1.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='entityagent',
-    version='0.1.2',
+    version='0.1.3',
     description='Entity Agent: An AI assistant with platform interaction capabilities',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
